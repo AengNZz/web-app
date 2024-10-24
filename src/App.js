f911900ab8d5f40bb2e237d3a658ab12758eba84
Andrey <arturn3kgg@gmail.com>
2024-10-24 20:58:32 +0600

    init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/header';
 import Form from './components/Form/Form'
 import ProductList from './components/ProductList/ProductList'
 import { useTelegram } from './hooks/useTelegram';
-import { Route, Routers } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -17,10 +17,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routers>
+      <Routes>
         <Route index element={<ProductList />} />
         <Route path={'form'} element={<Form />} />
-      </Routers>
+      </Routes>
     </div>
   );
 }
