1f568bc864365819d91bad49260b849ba4d02224
Andrey <arturn3kgg@gmail.com>
2024-10-24 20:22:32 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
-import { useTelegram } from "../../hooks/useTelegram";
+import { useTelegram } from './hooks/useTelegram';
 import './App.css';
 
 function App() {
-  const { telegram, onToggleButton } = useTelegram();
+  
+  const {telegram, onToggleButton} = useTelegram();
 
   useEffect(() => {
     telegram.ready();
